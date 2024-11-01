f616339cde92fbbad1a726795167c484761fc5b0
Fanis <gfanis1999@mail.ru>
2024-11-01 19:28:35 +0300

    comment2

--- js/main.js
+++ js/main.js
@@ -1,43 +1,30 @@
-$(function() {
+$(document).ready(function() {
 
-    $('#js-register-form').validate({
-        focusCleanup: true,
-        focusInvalid: false,
+    // Modal
+    $("[data-modal]").click(function () {
+        let modalId = $(this).attr("data-modal")
 
-        rules: {
-            form_name: {
-                required: true
-            },
-
-            form_email: {
-                required: true,
-                email: true
-            },
-            form_tel: {
-                required: true,
-                digits: true
-            }
-        },
+        openModal(modalId)
     })
 
-});
+    function openModal(modalId){
+        $(`#${modalId}`).addClass("modal--active")
+    }
 
-$.extend( $.validator.messages, {
-    required: "Это поле необходимо заполнить.",
-    remote: "Пожалуйста, введите правильное значение.",
-    email: "Пожалуйста, введите корректный адрес электронной почты.",
-    url: "Пожалуйста, введите корректный URL.",
-    date: "Пожалуйста, введите корректную дату.",
-    dateISO: "Пожалуйста, введите корректную дату в формате ISO.",
-    number: "Пожалуйста, введите число.",
-    digits: "Пожалуйста, вводите только цифры.",
-    creditcard: "Пожалуйста, введите правильный номер кредитной карты.",
-    equalTo: "Пожалуйста, введите такое же значение ещё раз.",
-    extension: "Пожалуйста, выберите файл с правильным расширением.",
-    maxlength: $.validator.format( "Пожалуйста, введите не больше {0} символов." ),
-    minlength: $.validator.format( "Пожалуйста, введите не меньше {0} символов." ),
-    rangelength: $.validator.format( "Пожалуйста, введите значение длиной от {0} до {1} символов." ),
-    range: $.validator.format( "Пожалуйста, введите число от {0} до {1}." ),
-    max: $.validator.format( "Пожалуйста, введите число, меньшее или равное {0}." ),
-    min: $.validator.format( "Пожалуйста, введите число, большее или равное {0}." )
-} );
+    $(".modal").click(function () {
+        closeModal()
+    })
+
+    $("._close-modal").click(function () {
+        closeModal()
+    })
+
+    $(".modal__dialog").click(function (e) {
+        e.stopPropagation()
+    })
+
+
+    function closeModal() {
+        $('.modal').removeClass("modal--active")}
+
+})
